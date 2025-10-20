9bd441d511cfde4cfc1a9406c57396b87e78fa86
Lucifer-2606 <roysidharth1996@gmail.com>
2025-10-20 18:41:44 +0530

    added coach section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
+// First Slide
+
 let swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 3,
   spaceBetween: 1,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
 
   breakpoints: {
     1250: {
@@ -18,5 +20,33 @@ let swiper = new Swiper(".swiper", {
     320: {
       slidesPerView: 1,
     },
+  },
+});
+// Second Slide
+
+let swiper2 = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 1,
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
+
+  breakpoints: {
+    1250: {
+      slidesPerView: 3,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
